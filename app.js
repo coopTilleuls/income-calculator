8c48477c2fd5fbff5300f3106fc4776cd37e2fe7
manolis <laptit99@hotmail.fr>
2022-12-26 11:32:35 +0100

    handle Exception

--- app.js
+++ app.js
@@ -6,27 +6,34 @@ async function getDocument(url) {
     }
 }
 
+function PropertyException(message) {
+  this.message = message;
+  this.name = 'PropertyException: ';
+}
+
 function convertTableToObject(tablePosition, propertyNames) {
   const CSSselector = `table:nth-of-type(${tablePosition.toString()}) tbody tr`;
   const rows = document.querySelectorAll(CSSselector);
   const data = [];
   rows.forEach(function(row) {
-    let j = propertyNames.length;
+    const cellNumber = row.querySelectorAll('td').length;
+    if (cellNumber !== propertyNames.length) {
+      throw new PropertyException('Number of properties\' name does not match with number of cells in a table\' row');
+    }
     const obj = {};
-    for (let i = 1; i <= j; i++) {
+    for (let i = 1; i <= propertyNames.length; i++) {
       let cell = row.querySelector(`td:nth-child(${i.toString()})`);
       if (cell) obj[propertyNames[i - 1]] = cell.textContent;
     }
     data.push(obj);
   })
-  return data;
+  return data === [] ? 'erreur' : data;
 }
 
 getDocument('https://raw.githubusercontent.com/coopTilleuls/.github/main/profile/revenues/README.md')
 .then(function (mdFile) {
     const md = new markdownit();
     const html = md.render(mdFile);
-    // console.log(md.parse(mdFile));
     document.body.insertAdjacentHTML('afterbegin', html);
     
     /*
@@ -85,11 +92,9 @@ getDocument('https://raw.githubusercontent.com/coopTilleuls/.github/main/profile
     }
   })
   */
-  salaryData = convertTableToObject(1, ['title', 'salary']);
-  bonusData = convertTableToObject(2, ['year', 'amount']);
+  
 
-  console.log(salaryData);
-/*
+  /*
   const bonusByYear = document.querySelectorAll('table:nth-of-type(2) tr');
   const bonusData = [];
   bonusByYear.forEach(function (row) {
@@ -104,6 +109,14 @@ getDocument('https://raw.githubusercontent.com/coopTilleuls/.github/main/profile
     }
   })
   */
+  try {
+      salaryData = convertTableToObject(1, ['title', 'salary']);
+    } catch(e) {
+      console.error(e.name, e.message);
+    }
+  bonusData = convertTableToObject(2, ['year', 'amount']);
+
+  console.log(salaryData);
   console.log(bonusData);
 });
 
