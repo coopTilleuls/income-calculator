77b5d93ea183ed8a96a578f738dd08bd374f42bf
manolis <laptit99@hotmail.fr>
2023-01-04 14:11:11 +0100

    fix: Kevin's refactoring

--- app.js
+++ app.js
@@ -2,31 +2,27 @@
     try {
         const md = new markdownit();
         const parser = new DOMParser();
-        const mdFile = (await (await fetch('https://raw.githubusercontent.com/coopTilleuls/.github/main/profile/revenues/README.md')).text());
+        const response = await fetch('https://raw.githubusercontent.com/coopTilleuls/.github/main/profile/revenues/README.md');
+        const mdFile = await response.text();
         const documentParsed = parser.parseFromString(md.render(mdFile), 'text/html');
-        
-        const table1Selector = 'table:nth-of-type(1) tbody tr';
-        const table2Selector = 'table:nth-of-type(2) tbody tr';
-        
-        function convertTableToObject(document, tableSelector, propertyNames) {
-            const rows = document.querySelectorAll(tableSelector);
-            const rowsArr = Array.from(rows);
-            return rowsArr.map((row) => {
-                const firstProperty = row.querySelector('td:nth-child(1)').textContent;
-                const secondProperty = row.querySelector('td:nth-child(2)').textContent;
-                const obj = {
-                    [propertyNames[0]]: firstProperty,
-                    [propertyNames[1]]: secondProperty,
-                };
-                return obj; 
-            })
-        }
 
-        const salaries = convertTableToObject(documentParsed, table1Selector, ['title', 'salary']);
-        const bonuses = convertTableToObject(documentParsed, table2Selector, ['year', 'amount']);
+        const extractDataFromMarkdownTable = n => {
+            const $td = documentParsed.querySelectorAll(`table:nth-of-type(${n}) td`);
+            const data = {};
 
-        console.log(salaries);
-        console.log(bonuses);
+            for (let i = 0; i < $td.length; i++) {
+                if (i%2 === 1) {
+                    data[$td[i-1].textContent] = $td[i].textContent;
+                }
+            }
+
+            return data;
+        };
+
+        const salaries = extractDataFromMarkdownTable(1);
+        const bonuses = extractDataFromMarkdownTable(2);
+        const seniorityBonus = extractDataFromMarkdownTable(3);
+
     } catch(e) {
         console.error(e);
     }
